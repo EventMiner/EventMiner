86b0d39975a5cf9624f65caa54a142ac66aeeb29
Sebastian Hüttemann <shuettemann@hotmail.com>
2016-06-15 15:55:00 +0200

    added requirements.txt

--- run_extraction.py
+++ run_extraction.py
@@ -35,7 +35,7 @@ def test_remove_references():
 
 def test_start_extraction():
     e = Event()
-    e.read_file("test/test_articles/random_test.txt")
+    e.read_file("test/test_articles/world_war_test.txt")
     e.start_extraction()
 
 
