e00cfb858cde31c720159d791192291ca282474a
Sebastian Hüttemann <shuettemann@hotmail.com>
2016-06-22 18:06:16 +0200

    added rule 7d, added test-sentences

--- eventminer/event/event_extraction.py
+++ eventminer/event/event_extraction.py
@@ -187,7 +187,9 @@ def detect_time_range_after_keyword(definitions, resultset, sentence, time_index
                         time_index_2 = m
 
                         print resultset["rule_nr"]
-
+                        if resultset["rule_nr"] == "4":
+                            resultset["rule_name"] = "Range: Month_to_Month_Year"
+                            resultset["rule_nr"] = "7d"
                         if resultset["rule_nr"] == "3":
                             resultset["rule_name"] = "Range: Day_Month_Year_to_Month_Year"
                             resultset["rule_nr"] = "7c"
